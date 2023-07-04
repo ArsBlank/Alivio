2048069c70b91466ba6b615d4d34c1c27f217943
ArsBlank <arsensheidd@gmail.com>
2023-07-04 23:23:56 +0300

    commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function navToggle() {
+  document.getElementById("headerMenu").classList.toggle("nav-toggle--active");
+  document.getElementById("burger").classList.toggle("nav-toggle--active");
+}
